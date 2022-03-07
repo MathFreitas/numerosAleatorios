70de22d2b2c2caa8e8296be32eb87514248f905f
MathFreitas <matheus.freitas_santos@outlook.com>
2022-03-07 20:28:11 -0300

    estilização concluida

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, SafeAreaView, TouchableOpacity, StyleSheet} from 'react-native';
 
 const App = () => {
     return (
         <SafeAreaView style={style.container}>
-            <Text>0</Text>
+            <Text style={style.numero}>0</Text>
             <TouchableOpacity>
                 <Text>Gerar Número</Text>
             </TouchableOpacity>
@@ -18,6 +18,21 @@ const style = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    numero: {
+        fontSize: 24,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+    },
+    botao: {
+        backgroundColor: '#FFFFFF',
+        width: '80%',
+        paddingHorizontal: 5,
+        paddingVertical: 10,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
     }
 })
 
